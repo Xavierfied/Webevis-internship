556fea18737df1eccf95b2b3c7efa53dc8400769
Abdul Momin <a3dulmomin@gmail.com>
2026-03-25 14:34:02 +0500

    update

--- video_track_yolo/model/file_processor.py
+++ video_track_yolo/model/file_processor.py
@@ -4,13 +4,11 @@ import numpy as np
 
 from utils.obj_detector  import draw_bb
 from utils.segmenter     import draw_seg
-from utils.pose_profiler import draw_pose
 from utils.frame_stat import vid_stat
 
 MODES = {
     "detection":    draw_bb,
     "segmentation": draw_seg,
-    "pose":         draw_pose
 }
 
 ################################################################################################################
